c3da980d862dd28581d7fc572397db9ae05a5075
HooniRING <ssoh7079@gmail.com>
2026-09-16 15:13:32 +0900

    플레이어 넉백 관련 수정

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 public class PlayerStatus : MonoBehaviour
 {
     [SerializeField] private int maxHp = 100;
+    [SerializeField] private float hitDuration = 0.6f;
     [SerializeField] private Animator animator;
 
     private PlayerController controller;
 
     private int hp;
-    private bool isHit;
 
     public int Hp => hp;
     public int MaxHp => maxHp;
 
-    public bool IsHit => isHit;
+    public bool IsHit {  get; private set; }
     public bool IsDead {  get; private set; }
 
 
@@ -26,7 +26,7 @@ public class PlayerStatus : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        if (IsDead || isHit) return;
+        if (IsDead || IsHit) return;
 
         hp -= damage;
         hp = Mathf.Max(hp, 0);
@@ -44,10 +44,10 @@ public class PlayerStatus : MonoBehaviour
     }
     private IEnumerator Hit()
     {
-        isHit = true;
+        IsHit = true;
         animator.SetTrigger("Hit");
-        yield return new WaitForSeconds(0.8f);
-        isHit = false;
+        yield return new WaitForSeconds(hitDuration);
+        IsHit = false;
         controller.ResetAnimation();
     }
     private void Die()
