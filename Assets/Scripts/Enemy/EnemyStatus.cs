37a9febc80ee1ee9bafd26d9101f6976713c16a0
HooniRING <ssoh7079@gmail.com>
2026-09-16 18:40:46 +0900

    플레이어 근접 공격 판정 및 적 데미지 구현

---
+++ Assets/Scripts/Enemy/EnemyStatus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EnemyStatus : MonoBehaviour
+{
+    [SerializeField] private int maxHp = 30;
+
+    private int hp;
+
+    private void Awake()
+    {
+        hp = maxHp;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        hp = Mathf.Max(hp, 0);
+
+        //확인용
+        Debug.Log($"Enemy HP : {hp} / {maxHp}");
+
+        if (hp <= 0) Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAnimationEvent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerAnimationEvent : MonoBehaviour
+{
+    private PlayerCombat combat;
+
+    private void Awake()
+    {
+        combat = GetComponentInParent<PlayerCombat>();
+    }
+
+    public void AttackHit()
+    {
+        combat.AttackHit();
+    }
+    public void AttackEnd()
+    {
+        combat.AttackEnd();
+    }
+}
