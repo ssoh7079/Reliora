bb5b04b41f5ec5ce2e3ecfd774b7e72ba4f8cbf4
HooniRING <ssoh7079@gmail.com>
2026-09-16 14:55:21 +0900

    플레이어 기본 이동 및 애니메이션 구현

---
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    [SerializeField] private int maxHp = 100;
+    [SerializeField] private Animator animator;
+
+    private PlayerController controller;
+
+    private int hp;
+    private bool isHit;
+
+    public int Hp => hp;
+    public int MaxHp => maxHp;
+
+    public bool IsHit => isHit;
+    public bool IsDead {  get; private set; }
+
+
+    private void Awake()
+    {
+        controller = GetComponent<PlayerController>();
+        hp = maxHp;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (IsDead || isHit) return;
+
+        hp -= damage;
+        hp = Mathf.Max(hp, 0);
+
+        //확인용
+        Debug.Log($"Player HP : {hp} / {maxHp}");
+
+        if (hp <= 0)
+        {
+            Die();
+            return;
+        }
+
+        StartCoroutine(Hit());
+    }
+    private IEnumerator Hit()
+    {
+        isHit = true;
+        animator.SetTrigger("Hit");
+        yield return new WaitForSeconds(0.8f);
+        isHit = false;
+        controller.ResetAnimation();
+    }
+    private void Die()
+    {
+        IsDead = true;
+        animator.SetTrigger("Death");
+        //확인용
+        Debug.Log("Player Dead");
+    }
+
+
+    //테스트용
+    [ContextMenu("Test Damage")]
+    private void TestDamage()
+    {
+        TakeDamage(20);
+    }
+}
